244a1cf1c82698f982039ba289baf3dac718178d
DYK <dyk900502@163.com>
2016-04-19 11:42:10 +0800

    董曰凯修改AppDelegate文件

--- TeamTest/TeamTest/TeamTest/AppDelegate.m
+++ TeamTest/TeamTest/TeamTest/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    NSLog(@"dyk修改");
     return YES;
 }
 
